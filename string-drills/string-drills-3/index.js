12189d6de93814014203273b705eb58377215be5
Kevin Stamps <kstamps2484@gmail.com>
2017-04-28 17:32:56 -0600

    moved everything into a string-drills folder

---
+++ string-drills/string-drills-3/index.js
@@ -0,0 +1,31 @@
+function textNormalizer(text) {
+    var newText = text.toLowerCase().trim();
+    console.log('"' + newText + '"');
+    return newText;
+}
+
+
+/* From here down, you are not expected to
+ understand.... for now :)
+
+
+ Nothing to see here!
+
+ */
+
+
+
+// tests
+
+function testTextNormalizer() {
+    var text = "   let's GO SURFING NOW everyone is learning how   ";
+    var expected = "let's go surfing now everyone is learning how";
+    if (textNormalizer(text) === expected) {
+        console.log('SUCCESS: `textNormalizer` is working');
+    }
+    else {
+        console.log('FAILURE: `textNormalizer` is not working');
+    }
+}
+
+testTextNormalizer();
